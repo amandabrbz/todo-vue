b70cb11eaddf7ff2dc6c89b399025f941772a805
amandabrbz <amanda.brbz@hotmail.com>
2021-01-30 03:07:59 -0300

    limpa aplicação e adiciona header default

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>todo vue app</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  header {
+    height: 100px;
+    background: #4F7947;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: #F1AA26;
+  }
+</style>
