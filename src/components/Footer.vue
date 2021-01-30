59cb098b691178191e7cada5e70873991b068fe1
amandabrbz <amanda.brbz@hotmail.com>
2021-01-30 03:16:05 -0300

    adiciona footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <h5>Amanda 4 PostGrad Web Dev </h5>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  footer {
+    width: 100vw;
+    position: absolute;
+    bottom: 0;
+    height: 50px;
+    background: #4F7947;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h5 {
+    color: #F1AA26;
+  }
+</style>
